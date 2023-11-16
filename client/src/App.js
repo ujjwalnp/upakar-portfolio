b20cd622d3c7f6af898ecf1ad614b9f14ab91613
ujjwalnp <dhakalujjwal99@gmail.com>
2023-11-16 18:41:12 +0545

    feat(app): routes defined

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/css/App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Services from "./components/Services";
+// import Portfolio from './components/Portfolio';
+import ServiceContact from "./components/ServiceContact";
+// import Testimonial from './components/Testimonial';
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Services />
+      {/* <Portfolio /> */}
+      <ServiceContact />
+      {/* <Testimonial /> */}
+      <Contact />
+      <Footer />
     </div>
   );
 }
