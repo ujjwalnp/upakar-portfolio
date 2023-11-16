005fa601412157867285ec1f2085b9be02f75a11
ujjwalnp <dhakalujjwal99@gmail.com>
2023-11-16 12:35:50 +0545

    feat(footer): add footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+    faLinkedinIn,
+    faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import "../assets/css/footer.css";
+
+function FooterShow({ fullName, bio, email, linkedin, twitter }) {
+    return (
+        <footer className="footer">
+        <div className="footer-bg">
+          <div className="footer-container container grid">
+            <div>
+              <h1 className="footer-title">{fullName}</h1>
+              <span className="footer-subtitle">{bio}</span>
+            </div>
+            <ul className="footer-links">
+              <li>
+                <a href="#services" className="footer-link">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href="#portfolio" className="footer-link">
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a href="#contact" className="footer-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+  
+            <div className="footer-socials">
+              <a
+                href={`https://www.linkedin.com/in/${linkedin}/`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-social"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+              <a
+                href={`mailto:${email}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-social"
+              >
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+              <a
+                href={`https://www.twitter.com/${twitter}/`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-social"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </div>
+          </div>
+          <p className="footer-copy">By ujjwalnp - Code</p>
+        </div>
+      </footer>
+    );
+  }
+  // Dummy data
+  const dummyData = {
+    fullName: "Sajjan Dhakal",
+    bio: "I'm a hacker",
+    email: "example@example.com",
+    linkedin: "example",
+    twitter: "example",
+  };
+  
+  function Footer() {
+    const [footerData, setFooterData] = useState([]);
+  
+    useEffect(() => {
+      // Simulating an API call with dummy data
+      setFooterData(dummyData);
+    }, []);
+    return (
+      <FooterShow
+        fullName={footerData.fullName}
+        bio={footerData.bio}
+        email={footerData.email}
+        linkedin={footerData.linkedin}
+        facebook={footerData.facebook}
+        twitter={footerData.twitter}
+      />
+    );
+  }
+  
+  export default Footer;
