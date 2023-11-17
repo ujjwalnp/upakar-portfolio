42ce9a9f1bda8bb9b9bd91562925f32a58ece8cb
ujjwalnp <dhakalujjwal99@gmail.com>
2023-11-17 20:40:17 +0545

    feat(app): setup express server & dependencies

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+/* EXPRESS SERVER  */
+const server = express()
+
+/* DB CONNECTION TO MONGO ATLAS */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database Connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+/* BODY PARSERS */
+server.use(express.json())
+
+/* MIDDLEWARES */
+server.use(cors())
+
+/* ROUTES */
+
+
+/* SERVER LISTEN */
+server.listen(process.env.SERVER_PORT, () => {
+  console.log("Server Started")
+})
