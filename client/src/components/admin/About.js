c5161c0a6d57a32a794d7aaf3b4ea7f5da0ef7cd
ujjwalnp <dhakalujjwal99@gmail.com>
2023-11-19 22:04:21 +0545

    feat(admin): about api integrated

--- client/src/components/admin/About.js
+++ client/src/components/admin/About.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import env from "react-dotenv";
+import Cookies from "js-cookie";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 
@@ -12,17 +16,85 @@ const dummyData = {
 };
 
 function About() {
+  const navigate = useNavigate();
+  const apiUrl = env.APP_API_BASE_URL;
+
+  const [about, setAbout] = useState("");
+  const [homeDescription, setHomeDescription] = useState("");
+  const [experiencedYears, setExperiencedYears] = useState("");
+  const [happyClients, setHappyClients] = useState("");
+  const [companiesWorked, setCompanyWorked] = useState("");
+  const [resume, setResume] = useState("");
+
+  const handleAboutChange = (e) => {
+    setAbout(e.target.value);
+  };
+  const handleHomeDescriptionChange = (e) => {
+    setHomeDescription(e.target.value);
+  };
+  const handleExperiencedYearsChange = (e) => {
+    setExperiencedYears(e.target.value);
+  };
+  const handleHappyClientsChange = (e) => {
+    setHappyClients(e.target.value);
+  };
+  const handleCompaniesWorkedChange = (e) => {
+    setCompanyWorked(e.target.value);
+  };
+  const handleResumeChange = (e) => {
+    setResume(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateAbout();
+  };
+
+  const updateAbout = async() => {
+    try {
+        const endpoint = `/api/user/${ Cookies.get('userId') }/`;
+
+        const response = await axios.patch(apiUrl + endpoint, {
+            about: {
+                about: about,
+                homeDescription: homeDescription,
+                experiencedYears: experiencedYears,
+                happyClients: happyClients,
+                companiesWorked: companiesWorked,
+                resume: resume
+            }
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${ Cookies.get('token') }`,
+            },
+        }
+        );
+
+        if (response.data.status === "error") {
+            // Reset the values on unsuccessful authentication
+            alert(response.data.error);
+            return;
+        }
+        navigate('/admin')
+    }
+    catch (error) {
+        console.log(error);
+    }
+  }
   return (
     <section className="about section" id="about">
       <h2 className="section-title">About</h2>
       <span className="section-subtitle">Professional Info</span>
 
-      <form className="container">
+      <form className="container" onSubmit={handleSubmit}>
         <div className="field">
           <input
             type="text"
             name="about"
             className="input"
+            value={about}
+            onChange={handleAboutChange}
             placeholder=" "
           />
           <label htmlFor="about" className="label">
@@ -34,6 +106,8 @@ function About() {
             type="text"
             name="homeDescription"
             className="input"
+            value={homeDescription}
+            onChange={handleHomeDescriptionChange}
             placeholder=" "
           />
           <label htmlFor="homeDescription" className="label">
@@ -45,6 +119,8 @@ function About() {
             type="text"
             name="experienced_years"
             className="input"
+            value={experiencedYears}
+            onChange={handleExperiencedYearsChange}
             placeholder=" "
           />
           <label htmlFor="experienced_years" className="label">
@@ -56,6 +132,8 @@ function About() {
             type="text"
             name="happy_clients"
             className="input"
+            value={happyClients}
+            onChange={handleHappyClientsChange}
             placeholder=" "
           />
           <label htmlFor="happy_clients" className="label">
@@ -67,10 +145,25 @@ function About() {
             type="text"
             name="company_worked"
             className="input"
+            value={companiesWorked}
+            onChange={handleCompaniesWorkedChange}
             placeholder=" "
           />
           <label htmlFor="company_worked" className="label">
             Company Worked
+          </label>
+        </div>
+        <div className="field">
+          <input
+            type="text"
+            name="resume"
+            className="input"
+            value={resume}
+            onChange={handleResumeChange}
+            placeholder=" "
+          />
+          <label htmlFor="resume" className="label">
+            Resume Link
           </label>
         </div>
 
