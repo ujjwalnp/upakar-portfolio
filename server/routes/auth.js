67cec9ad2ab7ed94c97acaca0020edb671460401
ujjwalnp <dhakalujjwal99@gmail.com>
2024-02-04 15:10:18 +0545

    feat(auth): add rateLimiter

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth')
+const rateLimiter = require('../middlewares/rateLimiter')
 
 router
   /* LOGIN_USER API */
-  .post('/', authController.loginUser)
+  .post('/', rateLimiter , authController.loginUser)
 //   .post('/signup', require('../controllers/createUser').createUser)
 
 
